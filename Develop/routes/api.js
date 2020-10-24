3a3f99d1c902887ed4fa51496093549711fdb88b
kdoh52 <kdoh52@gmail.com>
2020-10-24 14:42:09 -0700

    setup + route to dashboard

---
+++ Develop/routes/api.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const Transaction = require("../models/workout");
+const path = require('path');
+
+router.post("/api/transaction", ({ body }, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/transaction/bulk", ({ body }, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+})
+
+module.exports = router;
